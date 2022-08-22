e6b0eec39973da16102642af8db0ccb7b9837da7
Patan Moulali <patan.moulali@mphasis.com>
2022-08-22 14:56:53 +0530

    Add project files.

---
+++ 22ps1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22ps1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i;
+            Console.Write("\n\nRead and Print elements of an array:\n");
+            Console.Write("-----------------------------------------\n");
+
+            Console.Write("Input 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+        }
+    }
+}
